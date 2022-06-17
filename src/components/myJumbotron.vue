618a28bdb775278772997b69f13969ee4c3d9fdb
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-06-17 17:13:15 +0200

    jumbo finito

--- src/components/myJumbotron.vue
+++ src/components/myJumbotron.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-        <div class="col-12 justify-content-center align-items-center" id="jumbo">
-            <div class="container">
+        <div class="col-12" id="jumbo">
+            <div class="container d-flex h-100">
 
-                <div class="row">
+                <div class="row justify-content-center align-self-center">
                         <div class="col-12">
-                        <h1>Inspirings Tomorrows Leader</h1>
+                        <h1 class="text-center">Inspirings Tomorrows Leader</h1>
                     </div>
                     <div class="col-12">
-                        <h2>The Future Belongs To Those Who See Possibilities Today</h2>
+                        <h2 class="text-center">The Future Belongs To Those Who See Possibilities Today</h2>
                     </div>
-                    <div class="col-12">
-                        <div class="row">
+                    <div class="col-6 mt-2">
+                        <div class="row justify-content-center">
                             <div class="col-3">
-                                <button>FIND A COURSE</button>
+                                <button class="orange-button">FIND A COURSE</button>
                             </div>
                             <div class="col-3">
-                                <button>APPLY NOW</button>
+                                <button id="azure-button">APPLY NOW</button>
                             </div>
                         </div>                 
                     </div>
@@ -44,5 +44,24 @@ export default {
         background-position: center;
         background-size: cover;
         color: white;
+        h1{
+            font-size: 70px;
+        }
+        h2{
+            font-size: 30px;
+        }
+        .orange-button{
+            width: 100%;
+            background-color: #ffab0b;
+            border: 1px solid #ffab0b;
+            padding: 5px;
+        }
+        #azure-button{
+            width: 100%;
+            background-color: #77e6fe;
+            border: 1px solid #77e6fe;
+            padding: 5px;
+        }
+        
     }
 </style>
